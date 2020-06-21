42a1af36428f41fdf4bd2c070e5fceae4ff970c4
JuanPabloMontoya <37849335+JuanPabloMontoya271@users.noreply.github.com>
2020-06-20 18:29:21 -0700

    Updated generate train
    I added the train and test file generation

--- generate_train.py
+++ generate_train.py
@@ -1,14 +1,23 @@
 import os
 
 image_files = []
+train_files = []
+test_files = []
 os.chdir(os.path.join("data", "obj"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
         image_files.append("data/obj/" + filename)
 os.chdir("..")
+train_files = image_files[:int(len(image_files)*.7)]
+test_files = image_files[:int(len(image_files)*.7)]
 with open("train.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
     outfile.close()
+with open("test.txt", "w") as outfile:
+    for image in test_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()    
 os.chdir("..")
